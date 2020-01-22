ea68a7764a874360793e8ba2eaf9015574af9e92
Administrator <158@163.com>
2020-01-22 16:53:05 +0800

    第3次提交

--- mybaitsPlus/src/test/java/com/jadd/testsql.java
+++ mybaitsPlus/src/test/java/com/jadd/testsql.java
@@ -50,5 +50,7 @@ public class testsql {
         System.out.println("1111111111");
         System.out.println("1111111111");
         System.out.println("1111111111");
+        System.out.println("1111111111");
+        System.out.println("1111111111");
     }
 }
