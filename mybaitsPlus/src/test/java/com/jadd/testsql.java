6f8092ee18e646ba6231a5ac5a6c1ba3156a8142
Administrator <158@163.com>
2020-01-23 11:21:52 +0800

    "parent测试版本回退 --提交222"

--- mybaitsPlus/src/test/java/com/jadd/testsql.java
+++ mybaitsPlus/src/test/java/com/jadd/testsql.java
@@ -83,4 +83,13 @@ public class testsql {
         System.out.println("clone 测试版本回退提交一");
         System.out.println("1111111111");
     }
+
+    @Test
+    public void fun0888(){
+        System.out.println("parent 测试版本回退提交222");
+        System.out.println("1111111111");
+        System.out.println("1111111111");
+        System.out.println("1111111111");
+        System.out.println("1111111111");
+    }
 }
