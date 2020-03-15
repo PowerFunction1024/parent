0a5d85dd01c016c4195db9ccc8403f52b97f2f0a
Administrator <158@163.com>
2020-03-15 22:32:19 +0800

    ss

--- mongodb/src/test/java/test02.java
+++ mongodb/src/test/java/test02.java
@@ -141,7 +141,7 @@ public class test02 {
         System.out.println("dev");
     }
     @Test
-    public void fun01(){
+    public void fun0113(){
 
         System.out.println("clone--1");
         System.out.println("clone--2");
